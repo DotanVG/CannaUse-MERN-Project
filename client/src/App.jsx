69303e232d620ec7572a8166411b922946a1c8b1
DotanVG <97849922+DotanVG@users.noreply.github.com>
2022-06-18 03:47:11 +0300

    Added a basic React client

---
+++ client/src/App.jsx
@@ -0,0 +1,14 @@
+
+
+function App() {
+  return (
+    
+    <div>
+      <h1>CannaUse</h1>
+      Create Components And Add Them Instead Of This DIV Inside App.jsx
+    </div>
+    
+  );
+}
+
+export default App;
