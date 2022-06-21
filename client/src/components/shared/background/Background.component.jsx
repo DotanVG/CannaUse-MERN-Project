8dc4365eccc1afaa470c795b29686eaf8ed2de90
DotanVG <97849922+DotanVG@users.noreply.github.com>
2022-06-21 17:55:15 +0300

    Added HomePage and Background  Components

---
+++ client/src/components/shared/background/Background.component.jsx
@@ -0,0 +1,10 @@
+import './background.styles.css'
+import React from "react";
+
+const Background = () => {
+    return (
+        <div className="main-background"></div>
+    )
+}
+
+export default Background;
---
+++ client/src/pages/home-page/HomePage.component.jsx
@@ -0,0 +1,12 @@
+import './home-page.styles.css'
+import React from 'react'
+import Background from '../../components/shared/background/Background.component';
+
+const HomePage = () => {
+    return (
+    <Background />
+    
+    )
+}
+
+export default HomePage;
