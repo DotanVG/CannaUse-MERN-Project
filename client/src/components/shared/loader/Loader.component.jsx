8565b3773304cf7a5d3c119252f2a42dc46e1cc3
DotanVG <97849922+DotanVG@users.noreply.github.com>
2022-06-26 14:14:06 +0300

    Modified Loader and Favicon

--- client/src/components/shared/loader/Loader.component.jsx
+++ client/src/components/shared/loader/Loader.component.jsx
@@ -3,7 +3,7 @@ import './loader.styles.css';
 const Loader = () => {
     return (
         <div className="loader-container">
-            <div className="loader">Loading...</div>
+            <img src="https://s3.ca-central-1.amazonaws.com/test-public-420/loader.png" className="loader" alt="LOADING..."></img>
         </div>
     );
 };
