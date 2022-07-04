11708a290e5c5403c68acdbce072b624f8715033
DotanVG <97849922+DotanVG@users.noreply.github.com>
2022-07-05 00:48:45 +0300

    Changed Loader image file source location

--- client/src/components/shared/loader/Loader.component.jsx
+++ client/src/components/shared/loader/Loader.component.jsx
@@ -1,9 +1,10 @@
 import './loader.styles.css';
+import leafLoader from '../../../images/leaf-loader.png';
 
 const Loader = () => {
     return (
         <div className="loader-container">
-            <img src="https://s3.ca-central-1.amazonaws.com/test-public-420/loader.png" className="loader" alt="LOADING..."></img>
+            <img src={leafLoader} className="loader" alt="LOADING..."></img>
         </div>
     );
 };
